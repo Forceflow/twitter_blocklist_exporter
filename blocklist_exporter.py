d915aa51bdf1c8e2d22b5ed96aa18d9561db6034
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2023-03-30 03:11:08 +0200

    Added mutelist export functionality

--- blocklist_exporter.py
+++ blocklist_exporter.py
@@ -2,6 +2,7 @@ import json
 import tweepy
 import os
 import datetime
+import argparse
 
 version="v0.1"
 
@@ -16,6 +17,22 @@ def load_keys():
 		print("Check keys.json.example for an example keys.json file")
 		print("Exiting.")
 		quit()
+
+# Output mutelist as HTML
+def output_html_mutelist(mutelist):
+	file = open("mutelist.html", "w")
+	file.write("<html><head><title>My Twitter Mutelist</title></head><body>")
+	now = datetime.datetime.now()
+	file.write("<h1>Twitter mutelist generated on " + now.strftime("%Y-%m-%d %H:%M:%S") + "</h1>")
+	file.write("<p>You have <strong>" + str(len(mutelist)) + "</strong> muted users.</p>")
+	file.write("<p>The following list contains their last known username and numeric Twitter ID.</p>")
+
+	file.write("<ul>")
+	for muted_user in mutelist:
+		line = "<li><a href=\"https://twitter.com/" + str(muted_user['screen_name']) + "\" target=\"_blank\">" + muted_user['screen_name']  + "</a> " \
+		"(" + "<a href=\"https://twitter.com/i/user/" + str(muted_user['userid']) + "\" target=\"_blank\">" + str(muted_user['userid']) + "</a>)</li>"
+		file.write(line)
+	file.write("</ul></body></html>")
 
 # Output blocklist as HTML
 def output_html_blocklist(blocklist):
@@ -53,9 +70,21 @@ def get_blocklist_from_API(api):
 		blocklist.append({ 'userid' : user.id, 'screen_name' : user.screen_name})
 	return blocklist
 
+def get_mutelist_from_API(api):
+	mutelist = []
+	for user in tweepy.Cursor(api.get_mutes, skip_status=True).items():
+		# We grab userid and screen_name
+		mutelist.append({ 'userid' : user.id, 'screen_name' : user.screen_name})
+	return mutelist
+
 def main():
 	print("Twitter Blocklist exporter " + version + " - https://github.com/Forceflow/twitter_blocklist_exporter")
 	print("---")
+
+	# Parse arguments
+	parser = argparse.ArgumentParser(prog='Twitter Blocklist exporter')
+	parser.add_argument('--mutelist', action='store_true')
+	args = parser.parse_args()
 
 	# Load settings from file
 	print("Loading Twitter API keys from file")
@@ -68,11 +97,20 @@ def main():
 	auth = tweepy.OAuth1UserHandler(keys["API_KEY"], keys["API_KEY_SECRET"], keys["ACCESS_TOKEN"], keys["ACCESS_TOKEN_SECRET"])
 	api = tweepy.API(auth)
 
-	# Grab blocklist
-	print("Grabbing blocklist")
-	blocklist = get_blocklist_from_API(api)
-	print(str(len(blocklist)) + " blocked accounts found. Writing to blocklist.html")
-	output_html_blocklist(blocklist)
+	if args.mutelist is True:
+		# Grab mutelist
+		print("Grabbing mutelist")
+		mutelist = get_mutelist_from_API(api)
+		print(str(len(mutelist)) + " muted accounts found. Writing to mutelist.html")
+		output_html_mutelist(mutelist)
+	else:
+		# Grab blocklist
+		print("Grabbing blocklist")
+		blocklist = get_blocklist_from_API(api)
+		print(str(len(blocklist)) + " blocked accounts found. Writing to blocklist.html")
+		output_html_blocklist(blocklist)
+
+	
 	print("Done. Exiting.")
 
 if __name__ == "__main__":
